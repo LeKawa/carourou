cdd7359ed49dca835b58ae8b079f3871e6121498
LeKawa <hector.travaille@hetic.net>
2018-01-29 14:25:02 +0100

    pour vicente

---
+++ data.js
@@ -0,0 +1,49 @@
+var data = {
+  categories: ['Works','About','Contact','introduction'],
+
+Works : [
+  {
+  title:'Sci-fi Project',
+  undertitle:'Minecraft Project',
+  description: 'blbllzjpajf ajif jaizfh aifh ahf aàh',
+  img: ['img/oko.jpg','img/oko.jpg','img/oko.jpg','img/oko.jpg','img/oko.jpg',]
+},
+{
+  title:'Sci-fi Project',
+  undertitle:'Minecraft Project',
+  description: 'blbllzjpajf ajif jaizfh aifh ahf aàh',
+  img: ['img/oko.jpg','img/oko.jpg','img/oko.jpg','img/oko.jpg','img/oko.jpg',]
+},
+{
+  title:'Sci-fi Project',
+  undertitle:'Minecraft Project',
+  description: 'blbllzjpajf ajif jaizfh aifh ahf aàh',
+  img: ['img/oko.jpg','img/oko.jpg','img/oko.jpg','img/oko.jpg','img/oko.jpg',]
+}
+],
+
+About : [{
+  title:'About',
+  undertitle:'Who am I ?',
+  description:'hat if there was an Empire-focussed short Star Wars animation, drawn with the crazy detail and shading of classic 80s anime that\'s all but vanished from Japan nowadays? <br /> Well, I tried my best. Apologies in advance for not living up to Venus Wars standards. <br /> Drawn and',
+  img: ['img/oko.jpg'],
+}
+],
+
+Contact: [{
+  title:'About',
+  undertitle:'Who am I ?',
+  description:'hat if there was an Empire-focussed short Star Wars animation, drawn with the crazy detail and shading of classic 80s anime that\'s all but vanished from Japan nowadays? <br /> Well, I tried my best. Apologies in advance for not living up to Venus Wars standards. <br /> Drawn and',
+  img: ['img/oko.jpg'],
+}
+],
+
+introduction: [{
+  title:'About',
+  undertitle:'Who am I ?',
+  description:'hat if there was an Empire-focussed short Star Wars animation, drawn with the crazy detail and shading of classic 80s anime that\'s all but vanished from Japan nowadays? <br /> Well, I tried my best. Apologies in advance for not living up to Venus Wars standards. <br /> Drawn and',
+  img: ['img/oko.jpg'],
+}
+],
+
+}
